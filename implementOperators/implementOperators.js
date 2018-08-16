22e433fd2f4f5b307e2a06ef90b282282bfe54a1
FatimaAlmostafa <ff242468@gmail.com>
2018-08-16 20:24:54 +0300

    implement

--- implementOperators/implementOperators.js
+++ implementOperators/implementOperators.js
@@ -1,4 +1,4 @@
-/*  Write three functions to replace the multiply, divide, and modulo operators. 
+/*  Write three functions to replace the multiply, divide, and modulo operators.
 The only operators you may only use in your solution are addition and subtraction.
 Your functions will only have to handle integer math.
 Note: Don’t put any complex math in the comments in your code, or the tests may fail.
@@ -7,10 +7,20 @@ multiply(5, 2) === 10
 divide(5, 2) === 2
 modulo(5, 2) === 1  */
 var multiply = function(x, y) {
+  var sum = 0;
+  for (var i = 0; i < Math.abs(y); i++) {
+    sum += x;
+  }
 
+  if (x < 0 && y < 0) {
+    return Math.abs(sum);
+  } else if (x < 0 || y < 0 ) {
+    return -sum;
+  } else {
+    return sum;
+  }
 
-
-}
+};
 
 
 var divide = function(x, y) {
