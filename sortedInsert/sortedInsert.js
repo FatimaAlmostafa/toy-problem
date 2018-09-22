1082354439823d9d30aa552f8d6304820ffb4301
FatimaAlmostafa <ff242468@gmail.com>
2018-09-22 14:54:21 +0300

    toyprblem

--- sortedInsert/sortedInsert.js
+++ sortedInsert/sortedInsert.js
@@ -1,14 +1,27 @@
 /*
-Given a sorted stack of integers (smallest integer at the top) and an element to insert, 
+Given a sorted stack of integers (smallest integer at the top) and an element to insert,
 write a function – sortedInsert(stack, element) –
 which inserts the element into the correct position of the stack.
 sortedInsert should manipulate the original stack and return the original stack.
-The stack will have the methods, isEmpty, peek, push, and pop. 
+The stack will have the methods, isEmpty, peek, push, and pop.
 You can also use count to view how many elements are in the stack.
 */
 
 function sortedInsert(stack, element) {
-  //
-  // TODO
-  //
-} 
+  /if (stack.length > 0 || element > stack[stack.length - 1]) {
+    stack.push(element);
+  } else {
+    temp = stack.pop();
+    sortedInsert(element, stack);
+    stack.push(temp);
+  }
+
+
+}
+function sortStack() {
+  if (stack.length > 0) {
+    temp = stack.pop();
+    sortStack();
+    sortedInsert(temp, stack);
+  }
+}
