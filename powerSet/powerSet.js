460756df68394c71e1d5125da8428500d3868e84
FatimaAlmostafa <ff242468@gmail.com>
2018-09-22 14:46:11 +0300

    toyprblem

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -1,12 +1,12 @@
-/* Return an array that contains the power set of a given string. 
+/* Return an array that contains the power set of a given string.
 The power set is a set of all the possible subsets,
 including the empty set.
 Make sure your code does the following:
-All characters in a subset should be sorted alphabetically, 
+All characters in a subset should be sorted alphabetically,
 and the array of subsets should be sorted alphabetically.
 Sets of the same characters are considered duplicates regardless of order and count only once,
 e.g. ‘ab’ and ‘ba’ are the same.
-Duplicate characters in strings should be ignored; for example, ‘obama’ should be evaluated as 
+Duplicate characters in strings should be ignored; for example, ‘obama’ should be evaluated as
 if it only contained one ‘a’. See the result below.
 Examples
 Input	       Output
@@ -21,6 +21,21 @@ string:
 string:
 "obama"  	[ "", "a", "ab", "abm", "abmo", "abo", "am", "amo", "ao", "b", "bm", "bmo", "bo", "m", "mo", "o" ]*/
 
-var powerSet = function(str) {
+function powerSet (string) {
+  var result = [];
+  var sortedString = string.split('').sort().join('');
 
-}	
+  result.push("");
+
+  var recurse = (str) => {
+    // if ()
+    for (var i = 0; i < str.length; i++) {
+
+      result.push(str.charAt(i));
+    }
+    return;
+  }
+  recurse(sortedString);
+
+  return result;
+}
